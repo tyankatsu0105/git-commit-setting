a45aefc5ca16d0701b2b823971e79e702652a9e5
山本 勝也 <frips.rysilufupe@gmail.com>
2018-08-24 01:55:05 +0900

    ci(text.txt): :+1: テスト
    テスト
    
    BREAKING CHANGE: hoge
    
    re #333

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,25 @@ module.exports = {
   rules: {
     'subject-case': [0],
     'body-leading-blank': [2, 'always'],
-    'footer-leading-blank': [0]
+    'footer-leading-blank': [0],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'breaking',
+        'other'
+      ]
+    ]
   }
 };
